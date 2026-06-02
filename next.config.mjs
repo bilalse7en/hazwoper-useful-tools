4da96ed4b518da2cb9fc260470a5d2da52731c73
bilalse7 <113914970+bilalse7@users.noreply.github.com>
2026-06-02 13:15:51 +0500

    chore: update Sentry organization slug in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -81,7 +81,7 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: 'my-own-19',
+  org: 'hazwoper-useful-tools',
   project: 'hazwoper-useful-tools',
 
   // Only print logs for uploading source maps in CI
